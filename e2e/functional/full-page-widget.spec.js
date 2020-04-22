1166665a18f9947356045c650ef524869943a87b
Antonio <antonio.fullone@typeform.com>
2020-04-22 15:47:52 +0200

    test(DIST-142): Embed functional test coverage (#14)
    test(DIST-142): Embed functional test coverage

--- e2e/functional/full-page-widget.spec.js
+++ e2e/functional/full-page-widget.spec.js
@@ -1,9 +1,17 @@
-const { setupIframeTesting } = require('../cypress-utils')
+const { setupIframeTesting, openAsMobile } = require('../cypress-utils')
 
 describe('Full Page Embed Widget', function () {
   const iframe = '#typeform-full'
   it('Loads correctly the Full Page widget', function () {
     cy.visit('full.html')
+    setupIframeTesting(iframe)
+  })
+})
+
+describe('Full Page Embed Widget on Mobile', function () {
+  const iframe = '#typeform-full'
+  it('Loads correctly the Full Page widget', function () {
+    openAsMobile('full.html')
     setupIframeTesting(iframe)
   })
 })
