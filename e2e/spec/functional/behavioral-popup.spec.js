76844715e8960dce877fee2ef9e633c41ec526ae
Matej Lednicky <matej.lednicky@typeform.com>
2020-07-16 14:29:57 +0200

    feat(DIST-301): Add functional tests for behavioral triggers from DIST-193
    Test embeds created via embed code and API

---
+++ e2e/spec/functional/behavioral-popup.spec.js
@@ -0,0 +1,92 @@
+import { IFRAME_SELECTOR, open } from '../../cypress-utils'
+
+const pages = {
+  '-code': 'embed code',
+  '-api': 'API'
+}
+
+const getPageUrl = (name, suffix, query = '') => {
+  return `/behavioral${suffix}/${name}${suffix}.html?${query}`
+}
+
+Object.keys(pages).forEach(pageSuffix => {
+  describe(`Behavioral Embeds using ${pages[pageSuffix]}`, () => {
+    describe('Open: exit', () => {
+      before(() => {
+        open(getPageUrl('exit', pageSuffix, 'threshold=100'))
+      })
+
+      it('should not display the popup on mouse movement in the area downwards', () => {
+        cy.get('body')
+          .trigger('mousemove', { clientY: 50 })
+          .trigger('mousemove', { clientY: 55 })
+          .trigger('mousemove', { clientY: 60 })
+        cy.get(IFRAME_SELECTOR).should('not.exist')
+      })
+
+      it('should not display the popup on mouse movement outside the area', () => {
+        cy.get('body')
+          .trigger('mousemove', { clientY: 120 })
+          .trigger('mousemove', { clientY: 101 })
+        cy.get(IFRAME_SELECTOR).should('not.exist')
+      })
+
+      it('should display the popup on mouse movement in the area upwards', () => {
+        cy.get('body')
+          .trigger('mousemove', { clientY: 90 })
+          .trigger('mousemove', { clientY: 80 })
+          .trigger('mousemove', { clientY: 70 })
+        cy.get(IFRAME_SELECTOR).should('be.visible')
+      })
+    })
+
+    describe('Open: load', () => {
+      before(() => {
+        open(getPageUrl('load', pageSuffix))
+      })
+
+      it('should display the popup', () => {
+        cy.get(IFRAME_SELECTOR).should('be.visible')
+      })
+    })
+
+    describe('Open: scroll', () => {
+      before(() => {
+        open(getPageUrl('scroll', pageSuffix, 'percent=30'))
+      })
+
+      it('should not display the popup', () => {
+        cy.get(IFRAME_SELECTOR).should('not.exist')
+      })
+
+      it('should display the popup after scrolling down', () => {
+        cy.scrollTo(0, 950) // 950px is over 30% of the page height
+        cy.get(IFRAME_SELECTOR).should('be.visible')
+      })
+
+      it('should display the popup immediately after reloading the page on the same position', () => {
+        if (Cypress.isBrowser('chrome')) {
+          cy.reload()
+          cy.get(IFRAME_SELECTOR).should('be.visible')
+        }
+      })
+    })
+
+    describe('Open: time', () => {
+      const timeout = 1000 // the popup is opened after 1 seconds
+
+      before(() => {
+        open(getPageUrl('time', pageSuffix, `ms=${1000}`))
+      })
+
+      it('should not display the popup', () => {
+        cy.get(IFRAME_SELECTOR).should('not.exist')
+      })
+
+      it('should display the popup after 1 second', () => {
+        cy.wait(timeout)
+        cy.get(IFRAME_SELECTOR).should('be.visible')
+      })
+    })
+  })
+})
--- e2e/spec/functional/popup.spec.js
+++ e2e/spec/functional/popup.spec.js
@@ -16,36 +16,53 @@ const popupModes = {
   side_panel: 'Side Panel'
 }
 
+const pages = {
+  '/popup.html': 'embed code',
+  '/popup-api.html': 'API'
+}
+
 Object.keys(popupModes).forEach(popupMode => {
   describe(`${popupModes[popupMode]} Embed Widget`, () => {
-    describe('Desktop', () => {
-      before(() => {
-        open('/popup.html?foobar=hello')
-        openPopup(popupMode)
-      })
+    Object.keys(pages).forEach(pageUrl => {
+      describe(`Embedded using ${pages[pageUrl]} (at ${pageUrl})`, () => {
+        describe('Desktop', () => {
+          before(() => {
+            open(pageUrl)
+            openPopup(`#btn-${popupMode}`)
+          })
 
-      it('Passes hidden field parameter', () => {
-        cy.get(IFRAME_SELECTOR).should('have.attr', 'src').and('match', /foobar=hello/)
-      })
+          it('Passes hidden field parameter', () => {
+            cy.get(IFRAME_SELECTOR).should('have.attr', 'src').and('match', /foobar=hello/)
+          })
 
-      it('Closes Embed Popup clicking on the close button', () => {
-        closePopupViaButton()
-      })
+          it('Closes Embed Popup clicking on the close button', () => {
+            if (popupMode === 'popover' || popupMode === 'side_panel') {
+              closePopupViaButton(`#btn-${popupMode}`) // no close button, popup closes via the button that opened it
+            } else {
+              closePopupViaButton()
+            }
+          })
 
-      it('Closes Embed Popup using Keyboard', () => {
-        openPopup(popupMode)
-        closePopupViaKeyboard()
-      })
-    })
+          it('Closes Embed Popup using Keyboard', () => {
+            openPopup(`#btn-${popupMode}`)
+            if (popupMode === 'popover' || popupMode === 'side_panel') {
+              closePopupViaKeyboard(`#btn-${popupMode}`) // close button is used as fallback in firefox
+            } else {
+              closePopupViaKeyboard()
+            }
+          })
+        })
 
-    describe('Mobile', () => {
-      before(() => {
-        openOnMobile('/popup.html')
-        openPopup(popupMode)
-      })
+        describe('Mobile', () => {
+          before(() => {
+            openOnMobile(pageUrl)
+            openPopup(`#btn-${popupMode}`)
+          })
 
-      it('Closes Embed Popup clicking on close Button', () => {
-        closePopupViaButtonOnMobile()
+          it('Closes Embed Popup clicking on close Button', () => {
+            closePopupViaButtonOnMobile()
+          })
+        })
       })
     })
   })
