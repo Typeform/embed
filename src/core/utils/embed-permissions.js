0e3dc88ec6e2b270c520c18b3b03b894a7333354
Matej Lednicky <matej.lednicky@typeform.com>
2020-11-24 10:40:51 +0100

    feat: Allow fullscreen for embedded videos

--- src/core/utils/embed-permissions.js
+++ src/core/utils/embed-permissions.js
@@ -1,3 +1,3 @@
-const embedPermissions = 'camera; microphone; autoplay; encrypted-media;'
+const embedPermissions = 'camera; microphone; autoplay; encrypted-media; fullscreen;'
 
 export default embedPermissions
