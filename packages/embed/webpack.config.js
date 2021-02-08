bd909b7cfba879dbf8029466df28873468e7a25b
Marcelo Risoli <celorisoli@gmail.com>
2021-02-08 10:52:24 -0300

    chore: add webpack for embed browser build
    Add webpack, webpack-cli and ts-loadr as devDependencies for embed
    Add webpack config for browser bundle
    Add script to package.json to build with webpack

---
+++ packages/embed/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.ts',
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    library: 'embed-sdk',
+    libraryTarget: 'umd',
+    path: path.resolve(__dirname, 'dist/webpack'),
+  },
+}
