17145169dc530caecd5b3000b28bf5d476452841
Matej Lednicky <matej.lednicky@typeform.com>
2021-02-17 17:29:15 +0100

    feat: Add demo for sites served via webpack

---
+++ packages/demo-webpack/webpack.config.js
@@ -0,0 +1,17 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
