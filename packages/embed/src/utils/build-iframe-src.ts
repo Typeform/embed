903e4b8d774499ad9432bf0f859a76216a37624e
Matej Lednicky <matej.lednicky@typeform.com>
2021-02-19 14:56:47 +0100

    feat(DIST-701): Load embed options from element attributes

--- packages/embed/src/utils/build-iframe-src.ts
+++ packages/embed/src/utils/build-iframe-src.ts
@@ -1,5 +1,26 @@
 import { EmbedType, UrlOptions } from '../base'
 
-export const buildIframeSrc = (formId: string, _type: EmbedType, _options: UrlOptions) => {
-  return `https://form.typeform.com/to/${formId}`
+import { objectToQueryString } from './object-to-query-string'
+
+const typesToEmbed: Record<EmbedType, string> = {
+  widget: 'embed-widget', // TODO: when widget is full page use 'embed-fullpage'
+  popup: 'popup-blank',
+  slider: 'popup-drawer',
+  popover: 'popup-popover',
+  'side-tab': 'popup-side-panel',
+}
+
+const mapOptionsToQueryParams = (type: EmbedType, options: UrlOptions): Record<string, any> => ({
+  'typeform-embed': typesToEmbed[type],
+  'typeform-source': options.source,
+  'typeform-medium': options.medium,
+  'typeform-medium-version': options.mediumVersion,
+  'embed-hide-footer': options.hideFooter ? 'true' : undefined,
+  'embed-hide-headers': options.hideHeaders ? 'true' : undefined,
+})
+
+export const buildIframeSrc = (formId: string, type: EmbedType, options: UrlOptions): string => {
+  const queryParams = mapOptionsToQueryParams(type, options)
+  const queryString = objectToQueryString(queryParams)
+  return `https://form.typeform.com/to/${formId}?${queryString}`
 }
