e3edc9746381773b5c57f17d1fdd8185d9ce3d11
Matej Lednicky <matej.lednicky@typeform.com>
2021-02-19 15:01:00 +0100

    feat(DIST-701): Add functional tests and update demo

--- packages/embed/e2e/spec/functional/widget-window.spec.ts
+++ packages/embed/e2e/spec/functional/widget-window.spec.ts
@@ -1,14 +1,12 @@
-describe('Window', () => {
+describe('Widget Window', () => {
   before(() => {
     cy.visit('/widget-html.html')
   })
 
   it('should expose `tf` on `window`', () => {
-    // cy.window().should('have.property', 'tf')
     cy.window().then((win: any) => {
       expect(typeof win.tf).to.equal('object')
       expect(typeof win.tf.createWidget).to.equal('function')
-      expect(typeof win.tf.createPopup).to.equal('function')
     })
   })
 
