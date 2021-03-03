4d15e1b8974c8c5f20b5a7f0f77323943778f1b6
Marcelo Risoli <celorisoli@gmail.com>
2021-03-03 08:48:42 -0300

    test(DIST-710): Add more test to create-sidetab

--- packages/embed/src/factories/create-sidetab/create-sidetab.spec.ts
+++ packages/embed/src/factories/create-sidetab/create-sidetab.spec.ts
@@ -13,32 +13,57 @@ afterEach(() => {
   sidetab.unmount()
 })
 
-describe('create-sidetab', () => {
-  describe('#createPopup', () => {
-    describe('#open', () => {
-      it('should open', () => {
-        sidetab.open()
-        expect(screen.getByTestId('typeform-sidetab-wrapper')).toBeInTheDocument()
-      })
-
-      it('should show the icons', async () => {
-        expect(screen.getByTestId('default-icon')).toBeInTheDocument()
-        sidetab.open()
-        expect(screen.getByTestId('spinner-icon')).toBeInTheDocument()
-        jest.runAllTimers()
-        const iframe = screen.getByTestId('iframe')
-        fireEvent(iframe, new Event('load'))
-        expect(screen.getByTestId('close-icon')).toBeInTheDocument()
-      })
+describe('#createSidetab', () => {
+  describe('#open', () => {
+    it('should open', () => {
+      sidetab.open()
+      expect(screen.getByTestId('typeform-sidetab-wrapper')).toBeInTheDocument()
     })
 
-    describe('#close', () => {
-      it('should close', async () => {
-        sidetab.open()
-        jest.runAllTimers()
-        sidetab.close()
-        await waitForElementToBeRemoved(() => screen.queryByTestId('typeform-sidetab-wrapper'))
-      })
+    it('should show the icons', async () => {
+      expect(screen.getByTestId('default-icon')).toBeInTheDocument()
+      sidetab.open()
+      expect(screen.getByTestId('spinner-icon')).toBeInTheDocument()
+      jest.runAllTimers()
+      const iframe = screen.getByTestId('iframe')
+      fireEvent(iframe, new Event('load'))
+      expect(screen.getByTestId('close-icon')).toBeInTheDocument()
+    })
+  })
+
+  describe('#close', () => {
+    it('should close', async () => {
+      sidetab.open()
+      jest.runAllTimers()
+      sidetab.close()
+      await waitForElementToBeRemoved(() => screen.queryByTestId('typeform-sidetab-wrapper'))
+    })
+
+    it('should show the icons', async () => {
+      expect(screen.getByTestId('default-icon')).toBeInTheDocument()
+      sidetab.open()
+      expect(screen.getByTestId('spinner-icon')).toBeInTheDocument()
+      jest.runAllTimers()
+      const iframe = screen.getByTestId('iframe')
+      fireEvent(iframe, new Event('load'))
+      expect(screen.getByTestId('close-icon')).toBeInTheDocument()
+      sidetab.close()
+      await waitForElementToBeRemoved(() => screen.queryByTestId('typeform-sidetab-wrapper'))
+      expect(screen.getByTestId('default-icon')).toBeInTheDocument()
+    })
+  })
+
+  describe('#toggle', () => {
+    it('should open when closed', async () => {
+      sidetab.toggle()
+      expect(screen.getByTestId('typeform-sidetab-wrapper')).toBeInTheDocument()
+    })
+
+    it('should close when opened', async () => {
+      sidetab.open()
+      jest.runAllTimers()
+      sidetab.toggle()
+      await waitForElementToBeRemoved(() => screen.queryByTestId('typeform-sidetab-wrapper'))
     })
   })
 })
