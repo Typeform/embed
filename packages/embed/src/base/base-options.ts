980e470c4ec8ca3c5636144379ddae6b98874f76
Matej Lednicky <matej.lednicky@typeform.com>
2021-04-07 15:35:09 +0200

    feat(DIST-857): Allow embedding chat UI

--- packages/embed/src/base/base-options.ts
+++ packages/embed/src/base/base-options.ts
@@ -5,4 +5,11 @@ export type BaseOptions = {
    * @type {Record<string,string>}
    */
   hidden?: Record<string, string>
+
+  /**
+   * Display embedded typeform as conversation
+   *
+   * @type {boolean}
+   */
+  chat?: boolean
 }
