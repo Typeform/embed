fb60c63faafaef6145f69b4ce5acad7b1f10e86c
Marcelo Risoli <celorisoli@gmail.com>
2021-07-28 17:43:40 +0200

    feat(DIST-1090): Add full screen css handling

--- packages/embed/src/factories/create-widget/create-widget.ts
+++ packages/embed/src/factories/create-widget/create-widget.ts
@@ -1,4 +1,5 @@
 import { createIframe, hasDom, isFullscreen, unmountElement } from '../../utils'
+import { getWelcomeScreenHiddenHandler } from '../../utils/create-iframe/get-form-event-handler'
 
 import { WidgetOptions } from './widget-options'
 import { buildWidget } from './elements'
@@ -6,6 +7,14 @@ import { buildWidget } from './elements'
 export type Widget = {
   refresh: () => void
   unmount: () => void
+}
+
+const buildCloseButton = (close: () => void) => {
+  const closeButton = document.createElement('a')
+  closeButton.className = 'typeform-widget-close'
+  closeButton.innerHTML = '&times;'
+  closeButton.onclick = close
+  return closeButton
 }
 
 export const createWidget = (formId: string, options: WidgetOptions): Widget => {
@@ -19,13 +28,23 @@ export const createWidget = (formId: string, options: WidgetOptions): Widget => 
   const widgetOptions = options
   if (isFullscreen()) {
     widgetOptions.enableFullscreen = true
+    widgetOptions.forceTouch = true
   }
 
-  const { iframe } = createIframe(formId, 'widget', widgetOptions)
+  const { embedId, iframe } = createIframe(formId, 'widget', widgetOptions)
   const widget = buildWidget(iframe, options.width, options.height)
 
   options.container.innerHTML = ''
   options.container.append(widget)
+
+  if (widgetOptions.enableFullscreen) {
+    const { container } = options
+    window.addEventListener('message', getWelcomeScreenHiddenHandler(embedId, container))
+    const close = () => {
+      container.classList.remove('typeform-widget-fullscreen')
+    }
+    container.append(buildCloseButton(close))
+  }
 
   return {
     refresh: () => iframe.contentWindow?.location.reload(),
