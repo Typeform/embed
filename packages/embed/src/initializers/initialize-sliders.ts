8079deaf1562e6394995ca02721be99827faad25
Matej Lednicky <matej.lednicky@typeform.com>
2021-07-30 18:21:25 +0200

    feat(DIST-1126): Allow reloading and loading snippets in page
    When you inject embed snippet to the page later, you can load it manually via `window.tf.load()`.
    You can reloading all existing embed snippets in the page via `window.tf.reload()`.

--- packages/embed/src/initializers/initialize-sliders.ts
+++ packages/embed/src/initializers/initialize-sliders.ts
@@ -1,22 +1,10 @@
 import { createSlider } from '../factories/create-slider'
-import { includeCss } from '../utils'
 import { SLIDER_ATTRIBUTE } from '../constants'
 
-import { buildOptionsFromAttributes } from './build-options-from-attributes'
+import { initialize } from './initialize'
 
-export const initializeSliders = () => {
-  const sliderElements = document.querySelectorAll<HTMLElement>(`[${SLIDER_ATTRIBUTE}]`)
-
-  if (sliderElements.length > 0) {
-    includeCss('slider.css')
-  }
-
-  Array.from(sliderElements).forEach((button, index) => {
-    const formId = button.getAttribute(SLIDER_ATTRIBUTE)
-    if (!formId) {
-      throw new Error(`Invalid ${SLIDER_ATTRIBUTE}=${formId} for popup embed #${index}`)
-    }
-    const options = buildOptionsFromAttributes(button)
+export const initializeSliders = (forceReload: boolean = false) => {
+  initialize(SLIDER_ATTRIBUTE, 'slider.css', forceReload, (formId, options, button) => {
     const { toggle } = createSlider(formId, options)
     button.onclick = toggle
   })
