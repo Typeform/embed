e117c3313dc65d9389f764c99d05c22efbc83fad
antoniofull <antonio.fullone@typeform.com>
2021-11-16 15:22:27 +0100

    feat(DIST-1336): Add warning for non https websites
    feat(DIST-1336): Add about:srcdoc in allowed urls for https warning

--- packages/embed/src/factories/create-widget/elements/build-widget.ts
+++ packages/embed/src/factories/create-widget/elements/build-widget.ts
@@ -1,10 +1,12 @@
-import { setElementSize } from '../../../utils'
+import { createHttpWarningBanner, setElementSize } from '../../../utils'
 
 export const buildWidget = (iframe: HTMLIFrameElement, width?: number, height?: number): HTMLElement => {
   const widget = document.createElement('div')
   widget.className = 'typeform-widget'
 
   widget.append(iframe)
+  const httpWarningBanner = createHttpWarningBanner()
+  widget.append(httpWarningBanner)
 
   return setElementSize(widget, { width, height })
 }
