a39742b60ab147e01ee0e4e1222750585cc31ac1
Matej Lednicky <matej.lednicky@typeform.com>
2022-02-01 13:35:37 +0100

    chore(DIST-1443): Move dependencies into respective packages

--- packages/embed/.eslintrc.js
+++ packages/embed/.eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['@typeform/eslint-config', 'prettier', 'prettier/react', 'plugin:prettier/recommended'],
+  extends: ['@typeform/eslint-config', 'prettier', 'plugin:prettier/recommended'],
   rules: {
     'no-restricted-imports': ['error', { patterns: ['./../*'] }], // do not use unnecessary leading `./`
   },
