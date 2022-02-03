d2fa82db08b618b96e0f8be6fb6f2fab17e32ea0
Matej Lednicky <matej.lednicky@typeform.com>
2022-02-03 16:35:57 +0100

    fix(DIST-1443): Custom release script

---
+++ scripts/release.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+THIS_DIR="$(pwd)"
+REPO_ROOT="${GITHUB_WORKSPACE:=$THIS_DIR}"
+cd $REPO_ROOT
+echo "Repo root directory: $(pwd)"
+
+# release vanilla lib
+#cd $REPO_ROOT/packages/embed
+#yarn release-vanilla
+
+# bump vanilla lib
+cd $REPO_ROOT/packages/embed-react
+yarn upgrade @typeform/embed
+
+# commit vanilla lib bump
+cd $REPO_ROOT
+git add .
+git commit -m 'fix: Bump @typeform/embed dependency'
+git push origin
+
+# release react lib
+cd $REPO_ROOT/packages/embed-react
+yarn release
+
+# bump vanilla and react libs in demos
+cd $REPO_ROOT/packages/demo-nextjs
+yarn upgrade @typeform/embed
+yarn upgrade @typeform/embed-react
+
+cd $REPO_ROOT/packages/demo-react
+yarn upgrade @typeform/embed-react
+
+cd $REPO_ROOT/packages/demo-webpack
+yarn upgrade @typeform/embed
+
+# commit vanilla and react lib bumps in demos
+cd $REPO_ROOT
+git add .
+git commit -m 'chore: Bump @typeform/embed and @typeform/embed-react in demo packages'
+git push origin
