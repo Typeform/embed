06685f581b2c0fc135ac6aba0bd95679035e4e65
Matej Lednicky <matej.lednicky@typeform.com>
2022-04-13 16:42:03 +0200

    feat(DIST-1571): Allow setting iframe style directly as string

--- packages/embed/src/base/iframe-options.ts
+++ packages/embed/src/base/iframe-options.ts
@@ -1,3 +1,10 @@
+// per docs: https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/style
+// setting style as string is not advised, but perfectly valid approach
+interface StyleString {
+  style: string
+}
+type HTMLIFrameElementWithStyleString = Omit<HTMLIFrameElement, 'style'> & StyleString
+
 export type IframeOptions = {
-  iframeProps?: Partial<HTMLIFrameElement>
+  iframeProps?: Partial<HTMLIFrameElementWithStyleString>
 }
