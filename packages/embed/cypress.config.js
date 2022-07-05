c4de42801fff69ce3bb8ff29300afe3be6578e2d
Matej Lednicky <matej@lednicky.name>
2022-07-05 15:46:29 +0200

    chore(TYP-7505): Update Cypress version

---
+++ packages/embed/cypress.config.js
@@ -0,0 +1,24 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  chromeWebSecurity: false,
+  screenshotsFolder: './e2e/screenshots',
+  trashAssetsBeforeRuns: true,
+  viewportWidth: 1280,
+  viewportHeight: 1000,
+  animationDistanceThreshold: 2,
+  video: false,
+  eyesIsDisabled: false,
+  eyesFailCypressOnDiff: true,
+  eyesDisableBrowserFetching: false,
+  eyesPort: 61632,
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    specPattern: './e2e/spec/**/*.cy.{js,jsx,ts,tsx}',
+    baseUrl: 'http://localhost:8080/',
+  },
+})
