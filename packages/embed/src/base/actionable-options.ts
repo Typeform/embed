67998a84433585c61a08c4842ada515a8d2cf879
Marco Scarciofolo <marco.scarciofolo@typeform.com>
2022-10-10 16:31:05 +0200

    fix(DIST-1733): Question and height changed types

--- packages/embed/src/base/actionable-options.ts
+++ packages/embed/src/base/actionable-options.ts
@@ -5,18 +5,24 @@ export type ActionableOptions = {
   onReady?: () => void
   /**
    * Callback function that will be executed right after the typeform is successfully submitted.
-   * @param {string} responseId - Response ID string.
+   * @param {Object} payload - Event payload.
+   * @param {string} payload.responseId - Response ID string.
    * @param {string} response_id - DEPRECATED.
    */
   onSubmit?: (payload: { responseId: string }) => void
   /**
    * Callback function that will be executed once the typeform's active screen changes.
+   * @param {Object} event - Event payload.
+   * @param {string} event.ref - New question ref.
    */
-  onQuestionChanged?: (event: any) => void
+  onQuestionChanged?: (event: { ref: string }) => void
   /**
    * Callback function that will be executed once the typeform's active screen height changes.
+   * @param {Object} event - Event payload.
+   * @param {string} event.ref - New question ref.
+   * @param {number} event.height - New question height.
    */
-  onHeightChanged?: (event: any) => void
+  onHeightChanged?: (event: { ref: string; height: number }) => void
   /**
    * Callback function that will be executed once the Iframe close button is clicked.
    */
