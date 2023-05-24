7cbdc2c87f1a3a18d012644768e091fc342f6e0e
Pawel Gierlasinski <pawel.gierlasinski@typeform.com>
2023-05-24 10:56:34 +0200

    feat(REACH-374): Single Embed Code cypress test

---
+++ packages/embed/e2e/spec/functional/single-embed-code.cy.ts
@@ -0,0 +1,30 @@
+describe('Single Embed Code', () => {
+  describe(`Should load`, () => {
+    before(() => {
+      cy.intercept('https://api.typeform.com/forms/HLjqXS5W/embeds/embed-id', {
+        statusCode: 200,
+        body: {
+          type: 'widget',
+          config: {
+            html: `<div
+            id="wrapper"
+            data-tf-widget="HLjqXS5W"
+            data-tf-medium="demo-test"
+            data-tf-transitive-search-params="foo,bar"
+            data-tf-hidden="foo=foo value"
+            data-tf-tracking="utm_source=facebook"
+            data-tf-iframe-props="title=Foo Bar"
+          ></div>`,
+          },
+        },
+      })
+      cy.visit(`/embed-live.html`)
+    })
+
+    it('should display widget', () => {
+      cy.wait(500)
+      cy.get('.tf-v1-widget iframe').should('be.visible')
+      cy.get('.tf-v1-widget iframe').invoke('attr', 'src').should('contain', 'form.typeform.com/to/')
+    })
+  })
+})
