00f3a892972cd5d2e77411236724a63b4ac804c9
Rob Esp <robespmun@users.noreply.github.com>
2023-09-14 11:34:41 +0200

    feat(RESP-1781): Remove support for CUI (#613)
    * feat(RESP-1781): Remove support for CUI
    
    BREAKING CHANGE: CUI is not supported anymore
    
    * chore(RESP-1781): Update tsconfig to match standards
    
    * chore(RESP-1781): Update typeform eslint config to latest

--- packages/embed/src/base/base-options.ts
+++ packages/embed/src/base/base-options.ts
@@ -7,13 +7,6 @@ export type BaseOptions = {
   hidden?: Record<string, string>
 
   /**
-   * Display embedded typeform as conversation
-   *
-   * @type {boolean}
-   */
-  chat?: boolean
-
-  /**
    * Reopen the modal window with form in the same state as it wos when closed.
    * For widget this applies only on mobile (without displayOnMobile option).
    */
