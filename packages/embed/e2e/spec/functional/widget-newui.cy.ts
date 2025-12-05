f622f683d8c40d1545720e94c52f0279ef681db5
Joyce Van Herck <joyce.vherck@gmail.com>
2025-12-05 14:49:32 +0000

    chore: add tmp new RX tests (#706)

---
+++ packages/embed/e2e/spec/functional/widget-newui.cy.ts
@@ -0,0 +1,68 @@
+import { openOnMobile } from '../../cypress-utils'
+
+describe('Widget', () => {
+  testWidget('/widget-newui-html.html', 'html')
+  testWidget('/widget-newui-js.html', 'javascript')
+  testWidget('/', 'server-side rendering')
+})
+
+describe('Widget - Mobile', () => {
+  testMobile('/widget-newui-html.html', 'html')
+  testMobile('/widget-newui-js.html', 'javascript')
+})
+
+function testWidget(path: string, title: string) {
+  describe(`Widget - ${title}`, () => {
+    before(() => {
+      cy.visit(`${path}?foo=foo&bar=bar&baz=baz`)
+    })
+
+    it('should display widget', () => {
+      cy.get('.tf-v1-widget iframe').should('be.visible')
+      cy.get('.tf-v1-widget iframe').invoke('attr', 'src').should('contain', 'form.typeform.com/to/')
+    })
+
+    it('should pass options as query param', () => {
+      cy.get('.tf-v1-widget iframe')
+        .invoke('attr', 'src')
+        .should('contain', 'typeform-embed=embed-widget&typeform-source=localhost&typeform-medium=demo-test')
+    })
+
+    it('should pass hidden fields as hash', () => {
+      cy.get('.tf-v1-widget iframe').invoke('attr', 'src').should('contain', '#foo=foo+value')
+    })
+
+    it('should pass params from options to the iframe', () => {
+      cy.get('.tf-v1-widget iframe').invoke('attr', 'src').should('contain', 'bar=bar')
+    })
+
+    it('should not pass params not in the list to the iframe', () => {
+      cy.get('.tf-v1-widget iframe').invoke('attr', 'src').should('not.contain', 'baz=baz')
+    })
+
+    it('should pass additional iframe props', () => {
+      cy.get('.tf-v1-widget iframe').invoke('attr', 'title').should('equal', 'Foo Bar')
+    })
+  })
+}
+
+function testMobile(path: string, title: string) {
+  describe(`Widget - ${title}`, () => {
+    before(() => {
+      openOnMobile(`${path}?foo=foo&bar=bar&baz=baz`)
+    })
+
+    it('should reset the form when closing it', () => {
+      cy.get('iframe').then(($iframe) => {
+        const $body = $iframe.contents().find('body')
+        cy.wrap($body).find('[data-qa="ok-button"]').click()
+        cy.get('.tf-v1-widget-close').click()
+      })
+      cy.wait(1000)
+      cy.get('iframe').then(($iframe) => {
+        const $body = $iframe.contents().find('body')
+        cy.wrap($body).find('[data-qa="ok-button"]').should('exist')
+      })
+    })
+  })
+}
